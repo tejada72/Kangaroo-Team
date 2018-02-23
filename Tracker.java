f2328fa4b792f826fc67349ae7c1e0742e4647a5
brooklyn2187 <36543829+brooklyn2187@users.noreply.github.com>
2018-02-23 16:56:51 -0500

    Add files via upload
    Updated android documents with Android Manifest XML file. This contains some of the location tracking code, but it is not complete yet.

--- Tracker.java
+++ Tracker.java
@@ -1,17 +1,26 @@
 package teamkangaroo.areamonitoringtool;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Tracker extends AppCompatActivity
+public class Tracker extends AppCompatActivity implements LocationListener
 {
-    Button button;
-    TextView text;
+    Button button;      //Button for on/off button
+    TextView text;      //TextView for the Button Message
+    TextView xLabel;    //TextView for X coordinates
+    TextView yLabel;    //TextView for Y coordinates
     int buttonStatus = 1;
+
+    LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -20,6 +29,15 @@ public class Tracker extends AppCompatActivity
 
         button = (Button) findViewById(R.id.locationSwitchbtn);
         text = (TextView) findViewById(R.id.locationSwitchlbl);
+        xLabel = (TextView) findViewById(R.id.locationXtxt);
+        yLabel = (TextView) findViewById(R.id.locationYtxt);
+
+        /*
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        */
 
         //Turns location transmission off or on.
         button.setOnClickListener(new View.OnClickListener(){
@@ -47,5 +65,29 @@ public class Tracker extends AppCompatActivity
                 }
             }
         });
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider)
+    {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider)
+    {
+
     }
 }
