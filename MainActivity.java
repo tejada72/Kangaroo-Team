28654ec2e2731915a24d5b014469d7371e466af2
brooklyn2187 <36543829+brooklyn2187@users.noreply.github.com>
2018-03-23 13:44:01 -0400

    Add files via upload
    Uploaded files include a working login page.
    
    Things to still work on:
     - Merging with Harrison's Tracker
     - Error checking for login (needs JSON object checking)

--- MainActivity.java
+++ MainActivity.java
@@ -1,23 +1,164 @@
 package teamkangaroo.areamonitoringtool;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 //Get the MAC Address for user login to the operator.
+
+    EditText runID;
+    EditText userName;
+    String RUNID = null;
+    String USERNAME = null;
+    String run;
+    String user;
+
+    Context ctx = this;
+
+//"Run does not exist" function
+//Check the run.
+    //User puts in RunID.
+    //Check ID with URL.
+        //Continue if it exists.
+        //Ask again if it does not exist.
+    //private static HttpURLConnection con;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runID = (EditText) findViewById(R.id.runName);
+        userName = (EditText) findViewById(R.id.userName);
     }
 
     //User logs in
     public void login (View view)
     {
-        Intent intent = new Intent(this, Tracker.class);
-        startActivity(intent);
+        run = runID.getText().toString();
+        user = userName.getText().toString();
+        BackGround b = new BackGround();
+        b.execute(run, user);
+        //Intent intent = new Intent(this, Tracker.class);
+        //startActivity(intent);
+    }
+
+    class BackGround extends AsyncTask<String, String, String>
+    {
+        @Override
+        protected String doInBackground(String... params)
+        {
+            String runCode = params[0];        //runName is for runID, since runID is already taken
+            String username = params[1];
+            String data = "";
+            int temp;
+
+            try
+            {
+                URL url = new URL("http://ec2-54-157-62-1.compute-1.amazonaws.com/api/mobile.php");
+                String urlParams = "action=log-in&run-code=" + runCode + "&username=" + username;
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setDoOutput(true);
+                OutputStream os = httpURLConnection.getOutputStream();
+                os.write(urlParams.getBytes());
+                os.flush();
+                os.close();
+
+
+
+                InputStream is = httpURLConnection.getInputStream();
+
+                Reader in = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                StringBuilder sb = new StringBuilder();
+                for (int c; (c = in.read()) >= 0;)
+                    sb.append((char)c);
+                String response = sb.toString();
+                System.out.println(response);
+
+                try {
+                    JSONObject myResponse = new JSONObject(response.toString());
+                    if(myResponse.getBoolean("error")) {
+                        //Toast.makeText(ctx, myResponse.getString("error-msg"), Toast.LENGTH_SHORT).show();
+                        System.out.println("Not working");
+                    }
+                } catch (JSONException e){
+                    e.printStackTrace();
+                    return "Exception: " +e.getMessage();
+                }
+
+                while((temp=is.read()) != -1)
+                {
+                    data += (char)temp;
+                }
+
+                is.close();
+                httpURLConnection.disconnect();
+
+                return data;
+            }
+            catch (MalformedURLException e)
+            {
+                e.printStackTrace();
+                return "Exception: "+e.getMessage();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+                return "Exception: "+e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s)
+        {
+            String err = null;
+            try
+            {
+                JSONObject root = new JSONObject(s);
+                JSONObject user_data = root.getJSONObject("user_data");
+
+                RUNID = user_data.getString("run-code");
+                USERNAME = user_data.getString("username");
+                //PASSWORD = user_data.getString("password");
+                //EMAIL = user_data.getString("email");
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+                err = "Exception: "+e.getMessage();
+            }
+
+            Intent i = new Intent(ctx, Tracker.class);
+            //i.putExtra("run-code", RUNID);
+            //i.putExtra("username", USERNAME);
+            //i.putExtra("password", PASSWORD);
+            //i.putExtra("email", EMAIL);
+            //i.putExtra("err", err);
+            startActivity(i);
+        }
     }
 }
--- Tracker.java
+++ Tracker.java
@@ -6,10 +6,21 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.ContextCompat;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.location.Address;
+import android.location.Geocoder;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Tracker extends AppCompatActivity implements LocationListener
 {
@@ -20,10 +31,16 @@ public class Tracker extends AppCompatActivity implements LocationListener
     int buttonStatus = 1;
 
     LocationManager locationManager;
+    LocationListener locationListener;
+    double latitude;
+    double longitude;
+
+    Timer t = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
 
@@ -32,12 +49,31 @@ public class Tracker extends AppCompatActivity implements LocationListener
         xLabel = (TextView) findViewById(R.id.locationXtxt);
         yLabel = (TextView) findViewById(R.id.locationYtxt);
 
-        /*
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        double longitude = location.getLongitude();
-        double latitude = location.getLatitude();
-        */
+
+        if (buttonStatus == 1)
+        {
+            //Check permissions
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(getApplicationContext(),
+                            android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                                android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+
+
+            }
+        }
+        else
+        {
+            button.setText("Off");
+            button.setBackgroundColor(Color.RED);
+            text.setText("Idle");
+        }
+        getLocation();
 
         //Turns location transmission off or on.
         button.setOnClickListener(new View.OnClickListener(){
@@ -46,17 +82,22 @@ public class Tracker extends AppCompatActivity implements LocationListener
             {
                 switch (buttonStatus) {
 
-                    //Turn off
+                    //Turn on, if off
                     case 0:
-                        text.setText("Location is being transmitted");
+                        text.setText("Tracking");
                         button.setText("On");
                         button.setBackgroundColor(Color.GREEN);
                         buttonStatus = 1;
+                        //getLocation();
+                        //xLabel.setText("" + longitude);
+                        //yLabel.setText("" + latitude);
                         break;
-                    //Turn on
+                    //Turn off, if on
                     case 1:
-                        text.setText("Location is not being transmitted");
+                        text.setText("Idle");
                         button.setText("Off");
+                        xLabel.setText("");
+                        yLabel.setText("");
                         button.setBackgroundColor(Color.RED);
                         buttonStatus = 0;
                         break;
@@ -65,29 +106,67 @@ public class Tracker extends AppCompatActivity implements LocationListener
                 }
             }
         });
+
+        t.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                getLocation();
+                xLabel.setText("" + longitude);
+                yLabel.setText("" + latitude);
+            }
+        }, 0 , 10000);
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        xLabel.setText("Longitude:\n" + location.getLongitude());
+        yLabel.setText("Latitude:\n" + location.getLatitude());
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        }catch(Exception e)
+        {
+
+        }
+
+    }
+
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(Tracker.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onLocationChanged(Location location)
-    {
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
 
     @Override
-    public void onProviderDisabled(String provider)
-    {
+    public void onProviderEnabled(String provider) {
 
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras)
+
+    void getLocation()
     {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider)
-    {
-
+        try {
+            locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 50, 5, this);
+            //locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            longitude = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude();
+            latitude = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude();
+        }
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+        catch(NullPointerException e) {
+            e.printStackTrace();
+            longitude = 0;
+            latitude = 0;
+        }
     }
 }
