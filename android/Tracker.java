68a60a1f11b1deb8cc18448c6d96ab24f90785cb
tejada72 <36489202+tejada72@users.noreply.github.com>
2018-03-26 16:28:18 -0400

    Update Tracker.java

--- android/Tracker.java
+++ android/Tracker.java
@@ -1,5 +1,6 @@
 package teamkangaroo.areamonitoringtool;
 
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.IntentSender;
@@ -32,6 +33,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+
 public class Tracker extends AppCompatActivity implements LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     Button button;      //Button for on/off button
     TextView text;      //TextView for the Button Message
@@ -61,63 +63,17 @@ public class Tracker extends AppCompatActivity implements LocationListener, Goog
         locationClient = LocationServices.getFusedLocationProviderClient(this);
         //resolve = new ResolvableApiException();
 
-    if (ContextCompat.checkSelfPermission(Tracker.this, Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED) {
-        // Permission is not granted
-        // Need to prompt user with Android dialog that asks to use GPS
-        //Takes user to GPS settings
-        ActivityCompat.requestPermissions(Tracker.this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                1);
-    }
+        if (ContextCompat.checkSelfPermission(Tracker.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Need to prompt user with Android dialog that asks to use GPS
+            //Takes user to GPS settings
+            ActivityCompat.requestPermissions(Tracker.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }
 
-        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(Tracker.this)
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this).build();
-        googleApiClient.connect();
-
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(5 * 1000);
-        locationRequest.setFastestInterval(2 * 1000);
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-
-        //**************************
-        builder.setAlwaysShow(true); //this is the key ingredient
-        //**************************
-
-        PendingResult<LocationSettingsResult> result =
-                LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
-        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
-            @Override
-            public void onResult(@NonNull LocationSettingsResult result) {
-                final Status status = result.getStatus();
-//                final LocationSettingsStates state = result.getLocationSettingsStates();
-
-                switch (status.getStatusCode()) {
-                    case LocationSettingsStatusCodes.SUCCESS:
-
-
-                        break;
-                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                        // Location settings are not satisfied. But could be fixed by showing the user
-                        // a dialog.
-                        try {
-                            // Show the dialog by calling startResolutionForResult(),
-                            // and check the result in onActivityResult().
-                            status.startResolutionForResult(
-                                    Tracker.this, 1000);
-                        } catch (IntentSender.SendIntentException e) {
-                            // Ignore the error.
-                        }
-                        break;
-                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                        break;
-                }
-            }
-        });
+        checkGPS();
 
     /*if(((LocationManager)(getApplicationContext().getSystemService(Context.LOCATION_SERVICE))).isProviderEnabled(LocationManager.GPS_PROVIDER) == false) {
         try {
@@ -177,6 +133,7 @@ public class Tracker extends AppCompatActivity implements LocationListener, Goog
 
             public void onClick(View view)
             {
+                checkGPS();
                 switch (buttonStatus) {
 
                     //Turn off
@@ -198,6 +155,56 @@ public class Tracker extends AppCompatActivity implements LocationListener, Goog
                         buttonStatus = 0;
                         break;
                     default:
+                        break;
+                }
+            }
+        });
+    }
+
+    private void checkGPS() {
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(Tracker.this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).build();
+        googleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5 * 1000);
+        locationRequest.setFastestInterval(2 * 1000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        //**************************
+        builder.setAlwaysShow(true); //this is the key ingredient
+        //**************************
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(@NonNull LocationSettingsResult result) {
+                final Status status = result.getStatus();
+//                final LocationSettingsStates state = result.getLocationSettingsStates();
+
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+
+
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied. But could be fixed by showing the user
+                        // a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(
+                                    Tracker.this, 1000);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
                         break;
                 }
             }
